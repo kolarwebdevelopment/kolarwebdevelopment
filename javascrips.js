925ebee409593d67c9b24ec5da437716a5527c22
kolarwebdevelopment <kolarprogramming@gmail.com>
2024-11-30 13:22:25 +0100

    Update javascrips.js

--- javascrips.js
+++ javascrips.js
@@ -1,9 +1,1 @@
-const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
-window.onscroll = () => {
-  scrollToTopBtn.style.display = window.scrollY > 200 ? 'block' : 'none';
-};
-
-scrollToTopBtn.onclick = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
